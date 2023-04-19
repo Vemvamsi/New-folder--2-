74aa2083126427f912cfc8156d23aa0c9c7b31ea
Vemula sai vamsi krishna <v.saivamsikrishna007@gmail.com>
2023-04-19 12:36:05 +0530

    saisai

---
+++ areas.py
@@ -0,0 +1,11 @@
+Base=int(input("Enter the base:"))
+Height=int(input("Enter the height:"))
+area_of_triangle=(Base*Height)/2
+print(area_of_triangle)
+side=int(input("Enter the side:"))
+area_of_square=side**2
+print(area_of_square)
+length=int(input("Enter the length:"))
+breadth=int(input("Enter the breadth:"))
+area_of_rectangle=length*breadth
+print(area_of_rectangle)
